4d347fa75bc77855525276cad32609c920884349
Max Buckley <maxwbuckley@gmail.com>
2026-06-03 23:17:24 +0200

    chore: remove accidentally committed build_cuda/ artifacts

--- build_cuda/CMakeFiles/lightgbm_objs.dir/src/network/linkers_mpi.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lightgbm_objs.dir/src/network/linkers_mpi.cpp.o: \
- /mnt/c/Users/maxwb/Development/LightGBM/.claude/worktrees/optim/src/network/linkers_mpi.cpp \
- /usr/include/stdc-predef.h
